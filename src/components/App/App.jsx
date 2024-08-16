99e78e0b08da472daf4a65bae32a6721e3e72243
L0Bot <botterilouis@gmail.com>
2024-08-16 13:52:01 +0200

    before challenge 02

---
+++ src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MyTitle from "../MyTitle";
+
+function App() {
+  return (
+    <div>
+      <img src="/pokedex.png" alt="pokedex logo" />
+      <MyTitle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyTitle.jsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+  return <h1>Pokedex !</h1>;
+}
+
+export default MyTitle;
