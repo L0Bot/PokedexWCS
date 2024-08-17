c31763f458e97115072d25d13938e2935f9b950d
L0Bot <botterilouis@gmail.com>
2024-08-17 16:39:07 +0200

    ddone

--- src/components/Switcher/Switcher.jsx
+++ src/components/Switcher/Switcher.jsx
@@ -3,20 +3,19 @@ import PokemonCard from "../PokemonCard/PokemonCard";
 import NavBar from "../NavBar/NavBar";
 import styles from "./Switcher.module.scss";
 import { useReducer } from "react";
-const initialState = 0;
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "NEXT":
-      return state + 1;
-    case "PREVIOUS":
-      return state - 1;
-    default:
-      return state;
-  }
-};
 function Switcher({ pokemonList }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer((state, action) => {
+    switch (action.type) {
+      case "NEXT":
+        return state + 1;
+      case "PREVIOUS":
+        return state - 1;
+      default:
+        return state;
+    }
+  }, 0);
+
   return (
     <>
       <div className={styles.container}>
